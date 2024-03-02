0f88ba078b4e35265624144b9d6b2f96cfc23f5a
fantomg <386015805@qq.com>
2024-03-02 18:19:21 +0800

    commit

--- view.py
+++ view.py
@@ -2,14 +2,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import mne
 import numpy as np
-from mne.time_frequency import psd_array_multitaper, psd_array_welch
+from pyinform import transfer_entropy
+from mne.preprocessing import ICA
 
 matplotlib.use('TkAgg')
 
-filename = "./dataset/A02T.gdf"
+filename = "./dataset/A08T.gdf"
 
-raw_gdf = mne.io.read_raw_gdf(filename, stim_channel="auto", verbose='ERROR',
-                              exclude=(["EOG-left", "EOG-central", "EOG-right"]))
+raw_gdf = mne.io.read_raw_gdf(filename, stim_channel="auto", verbose='ERROR')
 
 # 将原通道重命名为10-20系统中，常用的64个电极位置
 raw_gdf.rename_channels(
@@ -48,7 +48,7 @@ raw_gdf.plot()
 plt.show()
 
 # 选择范围为Cue后 1s - 4s 的数据
-tmin, tmax = 1., 4.
+tmin, tmax = 1.5, 6.
 # 四类 MI 对应的 events_id
 event_id = dict({'769': 7, '770': 8, '771': 9, '772': 10})
 
@@ -59,7 +59,59 @@ print(epochs)
 labels = epochs.events[:, -1]
 # Get all epochs as a 3D array.
 data = epochs.get_data(copy=True)
-print(labels)
+print(labels.shape)
 print(data.shape)
-print(data)
 
+# 获取 EEG 通道名称列表
+eeg_channels = raw_gdf
+
+# 定义一个字典来存储计算结果
+TE_results = {}
+
+# 设置嵌入维度
+k = 1
+
+# 遍历每个通道计算传递熵
+for i, source_channel in enumerate(epochs):
+    for j, target_channel in enumerate(epochs):
+        if i != j:  # To avoid computing Transfer Entropy for the same channel
+            try:
+                TE = transfer_entropy(data[:, i, :], data[:, j, :], k)
+                key = f"{source_channel}_to_{target_channel}"
+                TE_results[key] = TE
+                print(f"Transfer Entropy from {source_channel} to {target_channel}: {TE}")
+            except Exception as e:
+                print(f"Error computing Transfer Entropy from {source_channel} to {target_channel}: {e}")
+
+
+
+# # 画出EEG通道图
+# epochs.plot()
+# plt.show()
+# # Perform Independent Component Analysis (ICA) to remove artifacts
+# ica = ICA(n_components=25, random_state=97, method="infomax")
+# ica.fit(raw_gdf)
+#
+# # ica.exclude = []
+# # Exclude components related to eye movements (EOG) or other artifacts
+# # eog_indices, _ = ica.find_bads_eog(epochs)
+# ica.exclude = [23, 24, 25]
+#
+# # plot diagnostics
+# ica.plot_properties(raw_gdf, picks=[23, 24, 25])
+#
+# # plot ICs applied to raw data, with EOG matches highlighted
+# ica.plot_sources(raw_gdf, show_scrollbars=False)
+
+# # Apply ICA to remove artifact components
+# ica.apply(raw_gdf)
+#
+# # Plot the cleaned EEG data
+# raw_gdf.plot()
+#
+# plt.show()
+# # Save the cleaned epochs data
+# # cleaned_epochs_file = 'cleaned_epochs-epo.fif'
+# # epochs.save(cleaned_epochs_file)
+#
+# # Further analysis or visualization can be performed on the cleaned epochs data
